f905354983ddd80a6f10cc008cd6aa7262ff6a7d
gvquiroz <gvquiroz@gmail.com>
2018-01-15 23:40:58 -0300

    fixed deps

--- countutils/count.go
+++ countutils/count.go
@@ -3,9 +3,9 @@ package countutils
 import (
 	"strings"
 	"unicode"
+	"golang.org/x/text/runes"
 	"golang.org/x/text/transform"
 	"golang.org/x/text/unicode/norm"
-	"golang.org/x/text/runes"
 )
 
 func CountChars(s string) map[string]int {
